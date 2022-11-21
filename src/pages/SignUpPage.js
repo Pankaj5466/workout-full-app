d109042250aa0100e59f4400dc6d0b99643c5838
Pankaj Kumar <pankaj@Pankajs-MacBook-Air.local>
2022-11-21 18:06:44 +0530

    Start: Creating signup page

---
+++ src/pages/SignUpPage.js
@@ -0,0 +1,13 @@
+
+const SignUpPage = ()=>{
+    return <forum>
+        <label>First Name: </label>
+        <input type = 'text'/>
+
+        <label>Last Name: </label>
+        <input type = 'text'/>
+    
+    </forum>
+}
+
+export default SignUpPage;
