b4df486378d04e174975a06946e69883b3452096
Pankaj Kumar <pankaj.54666@gmail.com>
2022-12-08 10:00:18 +0530

    InProgress: reorderign of dayPlan Exercise

--- ReactSteps.md
+++ ReactSteps.md
@@ -3,3 +3,4 @@
 - Make ExceriseView & pass excercise exerciseID as dynamic id -> Query Backend for info & then show loader screen + after data show the details exercise data. 
 
 - From ExerciseList page, clicking on any Excersie will change URL to of view & pass ID as parameter
+- Utilizing data-* attribute to set something in HTML elemtn(like ID or other data) & use them on some operation (ex: onClick)
--- src/components/dayPlan/CreateDayPlan.js
+++ src/components/dayPlan/CreateDayPlan.js
@@ -3,9 +3,12 @@ import Card from "../Card";
 import Modal from "../UI/Modal";
 import { useState } from "react";
 import ExerciseList from "../exercise/ExerciseList";
+import Button from "../UI/Button";
+import { components } from "react-select";
 const CreateDayPlan = (props) => {
     const [showAddUserModal,setShowAddUserModal] = useState(false);
-    const exerciseList = ['17097', '5678', '31520']; //list of excerciseID
+     
+    const [exerciseList,setExcericseList] = useState(['17097', '5678', '31520']); //list of excerciseID
 
     const addExcerciseHandler = (e) => {
         console.log('addExerciseHandler');
@@ -14,12 +17,36 @@ const CreateDayPlan = (props) => {
         //TO-DO: open ExcersieList page in pop-up mode, so user can search & select the desired exercize
     }
 
+    const handleReorderOperation = (type,eID)=>{
+        console.log(type,eID)
+        // const eID = e.currentTarget.getAttribute('data-eID'); //TO-DO: check why they are not working
+        // const eID2 = e.target.dataset.eID;
+        // const eID = e.target.getAttribute('data-eid');
+        
+        let newExerciseList  = exerciseList;
+        const idx = exerciseList.findIndex(id => id === eID);
+        console.log(idx);
+
+        newExerciseList = [...exerciseList.slice(0,idx),...exerciseList.slice(idx,idx+1),...exerciseList.slice(idx+1)];
+        console.log(newExerciseList);
+        // setExcericseList(newExerciseList);
+
+
+    }
     const dayPlanComponent = 
     <Card className="container flex justify-content-center">
-        
-        <ul>
-            {exerciseList.map(e => <SingleWorkoutViewRow eID={e} key={e} />)}
-        </ul>
+        <div className="d-flex">
+            <ul>
+                {exerciseList.map(eID => <div className="d-flex">
+                    <SingleWorkoutViewRow eID={eID} key={eID} />
+
+                    <Button key = {eID+'up'} data-eid={eID} onClick={()=>handleReorderOperation('up',eID)}>Up </Button>
+                    <Button key = {eID+'down'} data-eid={eID} onClick={()=>handleReorderOperation('down',eID)}>Down </Button>
+                </div>
+                )}
+            </ul>
+           
+        </div>
         <button className="btn btn-secondary justify-content-center"
             onClick={addExcerciseHandler}>Add</button>
     </Card>
@@ -37,7 +64,7 @@ const CreateDayPlan = (props) => {
     return (
         <>
         {dayPlanComponent}
-        { showAddUserModal && <Modal titel = 'Add User' content = {<ExerciseList/>} buttonName = 'Add Excerise(s)' onConfirm = {closeModalHandler}/>  }
+        { showAddUserModal && <Modal title = 'Select Exercise' content = {<ExerciseList/>} buttonName = 'Add Excerise(s)' onConfirm = {closeModalHandler}/>  }
         </>
     )
 }
