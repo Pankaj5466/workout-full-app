161f803b6da253da7427dbd54fc3f2852ec03aa9
Pankaj Kumar <pankaj.54666@gmail.com>
2022-12-08 09:11:14 +0530

    Modal with Overlay Added

---
+++ src/components/Card.js
@@ -0,0 +1,8 @@
+
+const Card = (props)=>{
+    return <div className={`card ${props.className}`}>
+        {props.children}
+        </div>
+}
+
+export default Card;
--- src/components/dayPlan/CreateDayPlan.js
+++ src/components/dayPlan/CreateDayPlan.js
@@ -1,27 +1,44 @@
 import SingleWorkoutViewRow from "../workout/SingleWorkoutViewRow";
+import Card from "../Card";
+import Modal from "../UI/Modal";
+import { useState } from "react";
+import ExerciseList from "../exercise/ExerciseList";
+const CreateDayPlan = (props) => {
+    const [showAddUserModal,setShowAddUserModal] = useState(false);
+    const exerciseList = ['17097', '5678', '31520']; //list of excerciseID
 
-const CreateDayPlan = (props) =>{
-    
-    const exerciseList = ['17097','5678','31520']; //list of excerciseID
-
-    const addExcerciseHandler =(e)=>{
+    const addExcerciseHandler = (e) => {
         console.log('addExerciseHandler');
+        setShowAddUserModal(true);
 
         //TO-DO: open ExcersieList page in pop-up mode, so user can search & select the desired exercize
     }
 
+    const dayPlanComponent = 
+    <Card className="container flex justify-content-center">
+        
+        <ul>
+            {exerciseList.map(e => <SingleWorkoutViewRow eID={e} key={e} />)}
+        </ul>
+        <button className="btn btn-secondary justify-content-center"
+            onClick={addExcerciseHandler}>Add</button>
+    </Card>
+
+    const closeModalHandler = ()=>{
+        console.log('close modal handler called\n');
+        setShowAddUserModal(false);
+
+        //TO-DO: Send the gather data to parent component
+
+    }
+
+    
+
     return (
         <>
-        <div className="container flex justify-content-center">
-            <p>CreateDayPlan Component</p>
-            <ul>
-                {exerciseList.map(e => <SingleWorkoutViewRow eID={e} key={e}/>)}
-            </ul>
-            <button className="btn btn-secondary justify-content-center"
-                onClick={addExcerciseHandler}>Add</button>
-        </div>
+        {dayPlanComponent}
+        { showAddUserModal && <Modal titel = 'Add User' content = {<ExerciseList/>} buttonName = 'Add Excerise(s)' onConfirm = {closeModalHandler}/>  }
         </>
-       
     )
 }
 
