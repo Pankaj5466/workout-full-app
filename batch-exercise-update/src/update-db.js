d3d728552ce09d2b6dc91645550b9adcd850cb39
= <pankaj.54666@gmail.com>
2023-01-20 23:00:33 +0530

    batch-update to db is working now

--- batch-exercise-update/src/update-db.js
+++ batch-exercise-update/src/update-db.js
@@ -1,6 +1,7 @@
 const db = require("./db");
 const fs = require('fs');
 const path = require('path');
+const chalk = require('chalk');
 
 // import db from './db/index.js';
 // import fs from 'fs';
@@ -22,23 +23,34 @@ const update = async () =>{
     console.log('ENTER: update');
     try{
 
-    const x = await db.query('SELECT * FROM user_table');
+    // const x = await db.query('SELECT * FROM user_table');
+    await db.query('TRUNCATE TABLE exercise_table');
     // console.log(x.rows);
     // return x.rows;
 
     const exerciseList = getExerciseObject();
 
-    for(let i=0;i<1;i++)
+    const l = 'lul';
+    for(let i=0;i<exerciseList.length;i++)
     {
         const obj = exerciseList[i];
-        console.log('will try to insert: ',obj);
+        console.log(chalk.blue.bgRed.bold('will try to insert: '),obj);
+
+        console.log('INSERTING\n');
+        const y = await db.query(
+            'INSERT INTO exercise_table(eID, title,subtitle,guide,img_href,img_alt,pmusclegroup,equipment,target_muscle_img,target_muslce_alt) VALUES($1, $2,$3,$4,$5,$6,$7,$8,$9,$10) RETURNING *',
+            [obj.eID,obj.pageTitle,obj.pageSubTitle,obj.guide,obj.img.href,obj.img.alt,obj.pMuscleGroup,obj.equipment,obj.targetMuscleImg?.src,obj.targetMuscleImg?.alt]
+        )
+
+        console.log(chalk.green.bgWhite.bold('INSERTED following: '));
+        console.log(y.rows);
     }
     // const y = await db.query('INSERT INTO exercise_table ')
 
     
     }catch(e){
         // throw e;
-        console.log(chalk.green('ERROR happened\n'));
+        console.log(chalk.red('ERROR happened\n'));
         console.log(e);
     }
 }
