45783ff22e9d3b6111c219f34aff595f7ac85380
= <pankaj.54666@gmail.com>
2023-01-21 11:39:12 +0530

    API to get single exercise details by passing eID

--- server/src/routes/exercise.js
+++ server/src/routes/exercise.js
@@ -1,13 +1,23 @@
 const express = require('express')
 const router = new express.Router();
-const exerciseList = require('../data/excercise-object')
+const db = require('../db/index')
+const exerciseList = require('../data/excercise-object');
 
-router.get('/run-batch',async (req,res,next)=>{
-    console.log('run-batch requested\n');
 
-    console.log(exerciseList);
-   
-     return res.status(200).send(exerciseList);
-})
+router.get('/get-exercise',async (req,res,next)=>{
+
+    console.log('ENTER: /get-exercise')
+    try
+    {
+        const eID = req.body.eID;
+        const x = await db.query('SELECT * FROM exercise_table WHERE eID = $1',
+            [eID]
+        );
+        return res.status(200).send(x.rows);
+    }catch(e){
+        console.log('ERROR happended during get\n');
+        res.status(500).send(e);
+    }
+});
 
 module.exports = router;
--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,14 +1,14 @@
 // ./routes/index.js
 const user = require('./userRoute')
 // const photos = require('./photos')
-const exerciseRoute = require('./exercise')
+const exercise = require('./exercise')
  
 module.exports = (app) => {
   app.use('/users', user);
+  app.use('/exercise',exercise);
 
   //app.use(user) -> will we at direct /
 //   app.use('/photos', photos)
 
-  app.use(exerciseRoute);
   // etc..
 }
