8aa0ad7b1ac0e784fa4aa3007e365ecc982c14f0
Pankaj Kumar <pankaj@192.168.1.19>
2023-05-09 19:24:54 +0530

    created project from start.spring.io

---
+++ server-three/src/main/java/com/exercises/app/exerciseappserver/ExerciseAppServerApplication.java
@@ -0,0 +1,13 @@
+package com.exercises.app.exerciseappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseAppServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseAppServerApplication.class, args);
+	}
+
+}
---
+++ server-three/src/test/java/com/exercises/app/exerciseappserver/ExerciseAppServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercises.app.exerciseappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseAppServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
