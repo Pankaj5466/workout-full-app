ddc5c7a1837657afa7186404ca8c48916214b1b0
Pankaj Kumar <pankaj@192.168.1.19>
2023-05-10 08:51:33 +0530

    returning response without creating pojo

--- server-three/src/main/java/com/exercises/app/exerciseappserver/controller/UserController.java
+++ server-three/src/main/java/com/exercises/app/exerciseappserver/controller/UserController.java
@@ -2,12 +2,15 @@ package com.exercises.app.exerciseappserver.controller;
 
 import com.exercises.app.exerciseappserver.entity.WebsiteUser;
 import com.exercises.app.exerciseappserver.repository.WebsiteUserSpringDataRepository;
+import com.fasterxml.jackson.databind.util.JSONPObject;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("user")
@@ -18,7 +21,7 @@ public class UserController {
 
     @RequestMapping(value="get",method= RequestMethod.GET)
     public String getuser() {
-        return "Hello World";
+        return "Hello Wohhh890rld";
     }
 
     @RequestMapping(value="create",method = RequestMethod.POST)
@@ -30,6 +33,16 @@ public class UserController {
         String lastName = request.getParameter("lastName");
         repository.save(new WebsiteUser(firstName,lastName));
 
-        return "success";
+        return "successh";
+    }
+
+    @RequestMapping(value="test",method = RequestMethod.GET)
+    public Map<String,String> test(){
+
+        return Map.of("test","test",
+                "test2","test2",
+                "test3","test393",
+//                "test4","test4",
+                "id","78");
     }
 }
