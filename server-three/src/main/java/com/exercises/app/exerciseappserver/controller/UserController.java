1e8e5b38543641d88e7653c25733aa761c1d1046
= <pankaj.54666@gmail.com>
2023-05-13 11:38:52 +0530

    formatting

--- server-three/src/main/java/com/exercises/app/exerciseappserver/controller/UserController.java
+++ server-three/src/main/java/com/exercises/app/exerciseappserver/controller/UserController.java
@@ -19,51 +19,47 @@ public class UserController {
     private WebsiteUserSpringDataRepository repository;
 
     //Class Functions Below
-    @RequestMapping(value="get",method= RequestMethod.GET)
+    @RequestMapping(value = "get", method = RequestMethod.GET)
     public String getuser() {
         return "Hello Wohhh890rld";
     }
 
     //TO-DO: Change return type to int , instead of whole user object
-//   @RequestMapping(value="create",method = RequestMethod.POST)
-    @PostMapping("create")
-    public WebsiteUser createUser(@RequestBody WebsiteUser user){
-
-
-//        System.out.println(request);
+    @PostMapping("create") //or @RequestMapping(value="create",method = RequestMethod.POST)
+    public WebsiteUser createUser(@RequestBody WebsiteUser user) {
 
         String firstName = user.getFirstName();
         String lastName = user.getLastName();
 
-        WebsiteUser savedUser =  repository.save(new WebsiteUser(firstName,lastName));
+        WebsiteUser savedUser = repository.save(new WebsiteUser(firstName, lastName));
         return savedUser; //TO-DO: return id instead of whole user object
     }
 
     //user/1
     @RequestMapping("{id}")
-    public WebsiteUser getUserById(@PathVariable String id){
+    public WebsiteUser getUserById(@PathVariable String id) {
         return repository.findById(Long.parseLong(id)).get();
     }
 
     //user/search?name=Pankaj&age=19
     @GetMapping("search")
-    public String searchUser(@RequestParam(name = "name", defaultValue = "Guest") String userName, @RequestParam int age){
+    public String searchUser(@RequestParam(name = "name", defaultValue = "Guest") String userName, @RequestParam int age) {
 
         return userName + " " + age;
     }
 
-    @RequestMapping(value="test",method = RequestMethod.GET)
-    public Map<String,String> test(){
+    @RequestMapping(value = "test", method = RequestMethod.GET)
+    public Map<String, String> test() {
 
-        return Map.of("test","test",
-                "test2","test2",
-                "test3","test393",
+        return Map.of("test", "test",
+                "test2", "test2",
+                "test3", "test393",
 //                "test4","test4",
-                "id","78");
+                "id", "78");
     }
 
     @RequestMapping("list")
-    public List<WebsiteUser> listUsers(){
+    public List<WebsiteUser> listUsers() {
         return repository.findAll();
 
     }
