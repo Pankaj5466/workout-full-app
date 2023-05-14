542da077ee48d5ee8166e7970be3328b24587a0a
= <pankaj.54666@gmail.com>
2023-05-14 17:07:57 +0530

    Start of mybatis project

---
+++ server-four-mybatis/src/main/java/com/exercise/app/exerciseappserver/ExerciseappserverApplication.java
@@ -0,0 +1,13 @@
+package com.exercise.app.exerciseappserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExerciseappserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExerciseappserverApplication.class, args);
+	}
+
+}
---
+++ server-four-mybatis/src/test/java/com/exercise/app/exerciseappserver/ExerciseappserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exercise.app.exerciseappserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExerciseappserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
